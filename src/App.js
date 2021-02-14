41c0645c6c3fea1807367afad88a49f9348c53c3
LalitMendha <lalitmendha102@gmail.com>
2021-02-14 18:18:58 +0530

    feat: counter Counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./styles.css";
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function onIncrement() {
+    setCount((count) => count + 1);
+  }
+  function onDecrement() {
+    // count = count + 1; This is not working it says count is read only
+    // setCount(count);
+    setCount((count) => count - 1);
+  }
+
+  return (
+    <div>
+      <div>{count}</div>
+      <br />
+      <div>
+        <button onClick={onIncrement}> + </button>
+        <button onClick={onDecrement}> - </button>
+      </div>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Counter />
     </div>
   );
 }
